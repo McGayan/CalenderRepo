9bb3e778eac16582aac607aa7688bb7d08f4ba5c
Ishasnath Gayan <ishanath.gayan@ancsis.com>
2025-01-01 09:09:35 +0530

    * buildpdf file changed to reflect the naming change of the source file * alignment change to prevent poyaday marker on Sundays intersecting with the cage right edge

--- buildpdf.bat
+++ buildpdf.bat
@@ -1,1 +1,1 @@
-gs   -o output.pdf   -sDEVICE=pdfwrite   -dPDFSETTINGS=/prepress   -dHaveTrueTypes=true   -dEmbedAllFonts=true   -dSubsetFonts=false   -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams"   -f test.ps
+gs   -o output.pdf   -sDEVICE=pdfwrite   -dPDFSETTINGS=/prepress   -dHaveTrueTypes=true   -dEmbedAllFonts=true   -dSubsetFonts=false   -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams"   -f generator.ps
